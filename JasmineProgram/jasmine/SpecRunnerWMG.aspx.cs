23654d16f804cdd348cf69195fbe45ac97d12317
Nasz <nasihere@gmail.com>
2015-09-08 19:21:40 -0700

    Jasmine Program

---
+++ JasmineProgram/jasmine/SpecRunnerWMG.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WF.EAI.Web.UCA.RetailBanking.Wealth.Views.TestNDebug.jasmine
+{
+    public partial class SpecRunnerWMG : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ViewState["json"] = Request.Form["json"];
+
+        }
+    }
+}
---
+++ JasmineProgram/jasmine/src/AutoScript.js
@@ -0,0 +1,447 @@
+﻿var AutoScriptParam = {
+    "applicationInfo": {
+        "isCoAppAvailable": "N",
+        "isTPBInd": "N",
+        "isSavedApp": "N",
+        "savedScreen": "",
+        "AUM": "9684584",
+        "jointApplicationApprovalInd": "",
+        "isNonSpouseAvailable": "",
+        "isWIDisclosureApplicable": "N",
+        "isWIDisclosureConfirmed": "",
+        "isAMLKYCApplicable": "Y",
+        "languagePref": "CHI",
+        "applicantInfoList": [
+            {
+                "applicantType": "P",
+                "applicantName": "CESAR DAVIS",
+                "applicationMethod": "F",
+                "inPersonInd": "Y",
+                "employer": "the company",
+                "jobTitle": "title",
+                "backgroundInfoQuesList": {
+                    "backgroundInfoQues": {
+                        "bankruptcyInd": "Y",
+                        "felonyInd": "Y",
+                        "lawsuitInd": "Y"
+                    }
+                },
+                "HMDA": {
+                    "Observation": "",
+                    "ethnicity": "",
+                    "gender": "",
+                    "race1": "",
+                    "race2": "",
+                    "race3": "",
+                    "race4": "",
+                    "race5": "",
+                    "race6": ""
+                },
+                "email": "lory.paul-jones@wellsfargo.com",
+                "maritalStatus": "M",
+                "domesticPartnerIndicator": ""
+            }
+        ],
+        "additionalContact": {
+            "name": "Nasir",
+            "phone": "3233005766",
+            "email": "nasihea@gmail.com"
+        },
+        "preferredTimeToCall": "M",
+        "applicantPhone": "323004752",
+        "businessTypeInfo": {
+            "trustIndividualCredit": "N",
+            "businessTaxId": "456345345",
+            "businessName": "Naseer",
+            "businessType": "Limited Liability Company",
+            "membersOrOfficersAll": "",
+            "guarantor": "",
+            "membersOrOfficersCorp": "",
+            "secretary": "",
+            "membersOfficersList": [
+                {
+                    "role": "Authorizer",
+                    "title": "Chairman of the Board",
+                    "name": "Naseer"
+                },
+                {
+                    "role": "Partner/Member",
+                    "title": "Chief Executive Officer",
+                    "name": "Naseeer"
+                },
+                {
+                    "role": "Partner/Member",
+                    "title": "Chief Executive Officer",
+                    "name": "Naseer"
+                },
+                {
+                    "role": "Authorizer",
+                    "title": "Chief Operation Officer",
+                    "name": "Naseer"
+                }
+            ]
+        },
+        "bankerInfo": {
+            "salesAgentUID": "gtst6068",
+            "bankerName": "CHRIS WRIGHT",
+            "officerID": "A0248",
+            "location": "00014",
+            "au": "4187",
+            "bankerPhone": "7012322082",
+            "comment": ""
+        },
+        "WealthData": {
+            "productList": [
+                
+                {
+                   
+                }
+            ],
+            "realEstateCollateralList": {
+                "realEstateCollateral": {
+                    "propertyAddress": {
+                        "addressLine": "1330 W 90TH AVE N",
+                        "city": "CONWAY SPRINGS",
+                        "state": "KS",
+                        "zip": "670318233"
+                    },
+                    "purchaseDate": "02/27/2015",
+                    "purchasePrice": "456",
+                    "estimatedValue": "45",
+                    "occupancyType": "S",
+                    "appCollateralFlag": "",
+                    "coAppCollateralFlag": "",
+                    "propertyType": "TH",
+                    "area": "45",
+                    "propertyUnderConstruction": "N",
+                    "trustVestedFlag": "N",
+                    "propertyFreeClearFlag": "Y",
+                    "propertyForSaleFlag": "N",
+                    "agricultureFlag": "",
+                    "explaination": "",
+                    "refinanceCompany": "",
+                    "mortgageInformation": {
+                        "mortgageList": [
+                          {
+                              "type": "MTG1",
+                              "mortgageType": "MTG1",
+                              "lender": "",
+                              "balance": "",
+                              "account": "",
+                              "monthlyPayment": ""
+                          },
+                          {
+                              "type": "MTGA",
+                              "mortgageType": "MTG1",
+                              "lender": "",
+                              "balance": "",
+                              "account": "",
+                              "monthlyPayment": ""
+                          }
+                        ],
+                        "anotherMortgageFlag": "",
+                        "refinancedFlag": "",
+                        "existingMortgageFlag": "",
+                        "payOffFlag": "",
+                        "propertyTaxFlag": "",
+                        "hoiFlag": ""
+                    },
+                    "hoiInformation": {
+                        "carrierName": "45645",
+                        "agentInformation": {
+                            "firstName": "",
+                            "lastName": "",
+                            "phoneNumber": ""
+                        }
+                    },
+                    "housingExpenses": {
+                        "hoaFees": "",
+                        "hoaFeesFreq": "",
+                        "propertyTaxesPayment": "456",
+                        "propertyTaxesPaymentFreq": "S",
+                        "monthlyMortgageHasTax": "N",
+                        "hoiPayment": "456",
+                        "hoiPaymentFreq": "A",
+                        "monthlyMortgagePayment": "N",
+                        "otherInsurancePayment": "",
+                        "otherInsurancePaymentFreq": "",
+                        "otherHoPayment": "",
+                        "otherHoPaymentFreq": ""
+                    },
+                    "comments": "credit detail comment"
+                }
+            },
+            "assetInfo": {
+                "propertTypeInd": "",
+                "selfPrepPFSInd": "N",
+                "ownPropertyInd": "N",
+                "totalAssets": "",
+                "totalLiabilities": "",
+                "totalNotesReceivable": "",
+                "totalREO": "",
+                "netWorth": "",
+                "assets": [
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  }
+                ],
+                "liabilities": [
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  },
+                  {
+                      "name": "Other Liability",
+                      "howHeld": "",
+                      "balance": "",
+                      "type": "label"
+                  }
+                ],
+                "notesReceivableList": [
+                  {
+                      "nameOfDebtor": "",
+                      "collateralType": "",
+                      "maturityDate": "",
+                      "annual": "",
+                      "unPaidBalance": "",
+                      "anchorclass": "",
+                      "error": "",
+                      "errorNo": ""
+                  }
+                ],
+                "realEstateOwnedList": []
+            },
+            "KYC": {
+                "completeOnline": "Y",
+                "eligibleInd": "Y",
+                "dueDiligenceInd": "",
+                "partiesIdentifiedInd": "Y",
+                "borrowConfirmedInd": "Y",
+                "grantorConfirmedInd": "Y",
+                "reqMetInd": "Y",
+                "level2CheckInd": "Y",
+                "riskDesignationInd": "Y",
+                "negNewsInd": "Y",
+                "reviewedApprvInd": "Y",
+                "sharedCreditInd": "Y",
+                "signatureInd": "Y"
+            },
+            "comments": "credit detail comment"
+        }
+    }
+};
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else if (i == key && obj[key] == val) {
+            objects.push(obj);
+        }
+    }
+    return objects;
+}
+function FillAutoModels() {
+    traverse(AutoScriptParam, process);
+    setTimeout(function () {
+        svpApp.util.buildSelects();
+    }, 100);
+
+}
+
+var ParentId = "";
+function process(key, value) {
+   // console.log(key + " : " + value);
+    if (typeof (value) == "object") {
+        ParentId = key;
+      //  console.log("--------------parent Now--------------" + ParentId)
+
+      //  console.log(value);
+    } else {
+     //   console.log("--------------attr--------------")
+      //  console.log("[ng-model*='" +  key + "']");
+        var obj = $("[ng-model*='" +  key + "'],[data-ng-model*='" +  key + "']");
+        if (obj) {
+         //   console.log("--------------Key : Value--------------")
+
+            //  $(obj).val(value);
+            getValues(Root, key, value);
+
+
+        }// else {
+          //  console.log("--------------Model Not Found --------------")
+        //}
+
+    }
+    //var filterNgModel = key.split(".");
+    //
+    //
+}
+
+function traverse(o, func) {
+    for (var i in o) {
+        func.apply(this, [i, o[i]]);
+        if (o[i] !== null && typeof (o[i]) == "object") {
+            //going on step down in the object tree!!
+            traverse(o[i], func);
+        }
+    }
+}
+
+
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else
+            //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+            if (i == key && obj[i] == val || i == key && val == '') { //
+                objects.push(obj);
+            } else if (obj[i] == val && key == '') {
+                //only add if the object is not already in the array
+                if (objects.lastIndexOf(obj) == -1) {
+                    objects.push(obj);
+                }
+            }
+    }
+    return objects;
+}
+
+//return an array of values that match on a certain key
+function getValues(obj, key,value) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key, value));
+        } else if (i == key) {
+            obj[i] = value;
+            //objects.push(obj[i]);
+        }
+    }
+    return objects;
+}
+
+//return an array of keys that match on a certain value
+function getKeys(obj, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getKeys(obj[i], val));
+        } else if (obj[i] == val) {
+            objects.push(i);
+        }
+    }
+    return objects;
+}
+$('html').bind('dblclick', function(event) {
+    if (event.ctrlKey && event.altKey) {
+        FillAutoModels();
+    }
+
+});
