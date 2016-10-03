feb28b8301176133f4275ddc12e8a535bab4aad2
Nasz <nasihere@gmail.com>
2016-10-02 23:07:32 -0700

    my first checkin

---
+++ myreact/myMicrotechSupportUsaInc/src/app/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class Contact extends React.Component{
+    render() {
+        return (
+            <h1>Contact Component</h1>
+        );
+    }
+}
+
---
+++ myreact/myMicrotechSupportUsaInc/src/app/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Header extends React.Component{
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Hello, world!</h1>
+                <p>...</p>
+                <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+        );
+    }
+}
+
